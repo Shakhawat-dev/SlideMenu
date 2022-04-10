86b2fc1b78af46e87d62d0e2cfd54d3ee1396f44
Md. Shakhawat Hossain Shahin <shakhawat.ch@gmail.com>
2022-04-10 12:36:56 +0600

    Initial Commit

---
+++ KavsoftSlideMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KavsoftSlideMenu
+//
+//  Created by Shakhawat Hossain Shahin on 10/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KavsoftSlideMenu/KavsoftSlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KavsoftSlideMenuApp.swift
+//  KavsoftSlideMenu
+//
+//  Created by Shakhawat Hossain Shahin on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct KavsoftSlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
